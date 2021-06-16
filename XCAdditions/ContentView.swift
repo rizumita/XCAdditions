08767ed9ca89d7ae64ed0eaee8d7cf61158d46e2
Ryoichi Izumita <r.izumita@caph.jp>
2021-06-16 10:17:43 +0900

    Initial Commit

---
+++ XCAdditions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCAdditions
+//
+//  Created by 和泉田 領一 on 2021/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCAdditions/XCAdditionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAdditionsApp.swift
+//  XCAdditions
+//
+//  Created by 和泉田 領一 on 2021/06/16.
+//
+
+import SwiftUI
+
+@main
+struct XCAdditionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
