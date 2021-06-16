72e2ff61e9ff0b264f8c5f9a515cc9a3c731230d
Ryoichi Izumita <r_izumita@yumemi.co.jp>
2021-06-16 10:22:56 +0900

    Create extension target

---
+++ XCAdditionsExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  XCAdditionsExtension
+//
+//  Created by 和泉田 領一 on 2021/06/16.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
---
+++ XCAdditionsExtension/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  XCAdditionsExtension
+//
+//  Created by 和泉田 領一 on 2021/06/16.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
